85c1973040f802b54e709ce3330a6675b08ac3c2
Rezowana Akter <rezowana10@gmail.com>
2021-11-19 22:38:18 +0600

    authentication added

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,11 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->text('image')->nullable();
+            $table->tinyInteger('gender')->comment('0 = Male, 1 = Female');
+            $table->date('dob')->nullable();
+            $table->double('latitude')->nullable();
+            $table->double('longitude')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
